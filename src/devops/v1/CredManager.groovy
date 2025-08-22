97835334b3480ea35795128988090ec90407b576
phu-phurithat <phurithat.komkham@gmail.com>
2025-08-22 14:51:23 +0700

    Add CredManager class with global environment configuration

---
+++ src/devops/v1/CredManager.groovy
@@ -0,0 +1,16 @@
+class CredManager {
+
+  def globalENV(){
+    env.TRIVY_BASE_URL = "http://trivy.trivy-system.svc.cluster.local:4954"
+    env.DEFECTDOJO_BASE_URL = "http://defectdojo-django.defectdojo.svc.cluster.local"
+    env.DOJO_KEY = "defect-dojo-key"
+    env.HARBOR_CRED = "harbor-cred"
+    env.JENKINS_CRED = "jenkins-cred"
+    env.GITLAB_NONPROD_KEY = "gitlab-nonprod-key"
+    env.GITLAB_PROD_KEY = "gitlab-prod-key"
+    env.HELM_PATH = "vayu-helm"
+    env.HELM_NONPROD_REPO = "https://gitlab.devopsnonprd.vayuktbcs/api/v4/projects/7410/packages/helm/stable"
+    env.OCP_NONPROD_AGENT = "ocp-nonprod-agent"
+    env.OCP_PROD_AGENT = "ocp-prod-agent"
+  }
+}
