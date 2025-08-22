59cc45ab3bd23ec192458e33fbb7fd398df59278
phu-phurithat <phurithat.komkham@gmail.com>
2025-08-22 14:52:13 +0700

    Remove globalENV method and rename COMPONENT_NAME to componentName

--- src/devops/v1/Preparer.groovy
+++ src/devops/v1/Preparer.groovy
@@ -3,21 +3,7 @@ package devops.v1
 class Preparer implements Serializable {
 
   def config
-  String COMPONENT_NAME
-
-  def globalENV(){
-    env.TRIVY_BASE_URL = "http://trivy.trivy-system.svc.cluster.local:4954"
-    env.DEFECTDOJO_BASE_URL = "http://defectdojo-django.defectdojo.svc.cluster.local"
-    env.DOJO_KEY = "defect-dojo-key"
-    env.HARBOR_CRED = "harbor-cred"
-    env.JENKINS_CRED = "jenkins-cred"
-    env.GITLAB_NONPROD_KEY = "gitlab-nonprod-key"
-    env.GITLAB_PROD_KEY = "gitlab-prod-key"
-    env.HELM_PATH = "vayu-helm"
-    env.HELM_NONPROD_REPO = "https://gitlab.devopsnonprd.vayuktbcs/api/v4/projects/7410/packages/helm/stable"
-    env.OCP_NONPROD_AGENT = "ocp-nonprod-agent"
-    env.OCP_PROD_AGENT = "ocp-prod-agent"
-  }
+  String componentName
 
   Preparer Preparer(args){
 
@@ -32,8 +18,13 @@ class Preparer implements Serializable {
 
   def readConfigs(repo) {
     echo "Reading configurations from ${repo}"
+<<<<<<< HEAD
     COMPONENT_NAME = repo.tokenize('/').last().replace('.git', '')
     String configPath = '${env.WORKSPACE}/${COMPONENT_NAME}/config.yaml'
+=======
+    componentName = repo.tokenize('/').last().replace('.git', '')
+    String configPath = env.WORKSPACE + componentName '/config.yaml'
+>>>>>>> 8de7786 (Remove globalENV method and rename COMPONENT_NAME to componentName)
     configContent = readFile(file: 'configPath', encoding: 'UTF-8')
 
     if (configContent) {
