c9c92ee25285c4b5e4c6fee5433727c03ca2e18d
Phurithat Komkham <99815333+phu-phurithat@users.noreply.github.com>
2025-09-03 14:17:11 +0700

    Put "prune" back

--- vars/microservicePipeline.groovy
+++ vars/microservicePipeline.groovy
@@ -100,6 +100,7 @@ def call(args) {
             buildctl \
               --addr ${BUILDKIT_ADDR} \
               build \
+              prune \
               --frontend dockerfile.v0 \
               --local context=. \
               --local dockerfile=. \
