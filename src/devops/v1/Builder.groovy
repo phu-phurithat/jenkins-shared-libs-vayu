00fb8ff3f2190adb1499565a2168a0e2b2ab5c77
Thun650710553 <116154739+Thun650710553@users.noreply.github.com>
2025-09-04 09:49:16 +0000

    fix env.SONAR_TOKEN

--- src/devops/v1/Builder.groovy
+++ src/devops/v1/Builder.groovy
@@ -1,7 +1,7 @@
 package devops.v1
 
-    def Compile(env.SONAR_TOKEN,SONAR_HOST,SONAR_PROJECT_KEY,language){
-        withCredentials([string(credentialsId: env.SONAR_TOKEN, variable: 'defectdojo_api_key')]){
+    def Compile(SONAR_TOKEN,SONAR_HOST,SONAR_PROJECT_KEY,language){
+        withCredentials([string(credentialsId: SONAR_TOKEN, variable: 'defectdojo_api_key')]){
             if(language=="maven"){
             sh """
                mvn clean install verify sonar:sonar \
