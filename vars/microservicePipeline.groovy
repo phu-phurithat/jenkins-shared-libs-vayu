1b1208ac2089c499fb1fb955bc2ae30cc8035025
Thun650710553 <116154739+Thun650710553@users.noreply.github.com>
2025-09-17 08:55:09 +0000

    debug

--- vars/microservicePipeline.groovy
+++ vars/microservicePipeline.groovy
@@ -67,8 +67,9 @@ def call(args) {
     dir('src') {
       stage('Checkout Microservice Code for Preparation') {
         microserviceRepo = config.kinds.deployments[args.MICROSERVICE_NAME]
-        fullPath = (microserviceRepo =~ /github\.com\/(.*)/).group(1)
-        component = fullPath.tokenize('/')[-1].replace('.git', '')
+        echo "TEST microserviceRepo = ${microserviceRepo}"
+        // fullPath = (microserviceRepo =~ /github\.com\/(.*)/).group(1)
+        // component = fullPath.tokenize('/')[-1].replace('.git', '')
         if (!microserviceRepo) {
           error "Microservice '${args.MICROSERVICE_NAME}' not found in configuration."
         }
