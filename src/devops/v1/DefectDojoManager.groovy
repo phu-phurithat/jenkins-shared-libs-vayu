9ffb20586947e02d27063083758217c92adc6fc1
Thun650710553 <116154739+Thun650710553@users.noreply.github.com>
2025-09-18 07:11:01 +0000

    add logic to check if product and engage is available

--- src/devops/v1/DefectDojoManager.groovy
+++ src/devops/v1/DefectDojoManager.groovy
@@ -1,5 +1,57 @@
 package devops.v1
+def productName=''
+def engagementName=''
 def ImportReport(fullPath, imageTag,component) {
+  productName= fullPath+'-'+component
+  engagementName= component':'+imageTag
+  
+  def productId = sh(
+                        script: """
+                            curl -s -k -H "Authorization: Token ${DOJO_KEY}" \
+                                 "${DEFECTDOJO_BASE_URL}/api/v2/products/?name=${productName}" \
+                                 | jq '.results[0].id'
+                        """,
+                        returnStdout: true
+                        ).trim()
+   if (productId == "null" || productId == "") {
+                        echo "Product not found. Creating..."
+                        productId = sh(
+                            script: """
+                                curl -s -k -X POST "${DEFECTDOJO_BASE_URL}/api/v2/products/" \
+                                    -H "Authorization: Token ${DOJO_KEY}" \
+                                    -H "Content-Type: application/json" \
+                                    -d '{"name": "${productName}", "description": "Auto-created from Jenkins","prod_type": 1}' \
+                                    | jq '.id'
+                            """,
+                            returnStdout: true
+                        ).trim()
+                    }
+
+  def engagementId = sh(
+                        script: """
+                            curl -s -k -H "Authorization: Token ${DOJO_KEY}" \
+                                 "${DEFECTDOJO_BASE_URL}/api/v2/engagements/?name=${engagementName}&product=${productId}" \
+                                 | jq '.results[0].id'
+                        """,
+                        returnStdout: true
+                       ).trim()
+   if (engagementId == "null" || engagementId == "") {
+                        echo "Engagement not found. Creating..."
+                        engagementId = sh(
+                            script: """
+                                curl -s -k -X POST "${DEFECTDOJO_BASE_URL}/api/v2/engagements/" \
+                                    -H "Authorization: Token ${DOJO_KEY}" \
+                                    -H "Content-Type: application/json" \
+                                    -d '{
+                                        "name": "${engagementName}",
+                                        "product": ${productId},
+                                        "status": "In Progress",
+                                        "engagement_type": "CI/CD"
+                                    }' | jq '.id'
+                            """,
+                            returnStdout: true
+                        ).trim()
+                    }
       withCredentials([string(credentialsId: DOJO_KEY, variable: 'DOJO_KEY')]) {
           //SonarQube Scan Source Code
           sh """
@@ -11,8 +63,8 @@ def ImportReport(fullPath, imageTag,component) {
             -F active="true" \
             -F verified="true" \
             -F file=@sonarqube-report.json \
-            -F product_name=${fullPath}-${component} \
-            -F engagement_name=${component}:${imageTag} \
+            -F product_name=${productName} \
+            -F engagement_name=${engagementName} \
             -F deduplication_on_engagement=true \
             -F close_old_findings=true \
             -F auto_create_context=true
@@ -26,11 +78,11 @@ def ImportReport(fullPath, imageTag,component) {
             -F active="true" \
             -F verified="true" \
             -F file=@trivy_deps.json \
-            -F product_name=${fullPath}-${component} \
-            -F engagement_name=${component}:${imageTag} \
+            -F product_name=${productName} \
+            -F engagement_name=${engagementName} \
             -F deduplication_on_engagement=true \
             -F close_old_findings=true \
-            -F auto_create_context=true
+            -F auto_create_context=trueengagementName
         """
 
         //Trivy Scan Image
@@ -42,8 +94,8 @@ def ImportReport(fullPath, imageTag,component) {
             -F active="true" \
             -F verified="true" \
             -F file=@trivy_image.json \
-            -F product_name=${fullPath}-${component} \
-            -F engagement_name=${component}:${imageTag} \
+            -F product_name=${productName} \
+            -F engagement_name=${engagementName} \
             -F deduplication_on_engagement=true \
             -F close_old_findings=true \
             -F auto_create_context=true
