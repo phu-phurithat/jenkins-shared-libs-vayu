39d882105c965702259e6ea3c99829492bfb5ef4
Phurithat Komkham <99815333+phu-phurithat@users.noreply.github.com>
2025-09-18 08:19:04 +0000

    fix readJson

--- src/devops/v1/DefectDojoManager.groovy
+++ src/devops/v1/DefectDojoManager.groovy
@@ -1,28 +1,28 @@
 package devops.v1
 
-def ImportReport(fullPath, imageTag,component) {
- String productName= "${fullPath}-${component}"
-String engagementName= "${component}:${imageTag}"
-  
-  // def productId = sh(
-  //                       script: """
-  //                           curl -s -k -H "Authorization: Token ${DOJO_KEY}" \
-  //                                "${DEFECTDOJO_BASE_URL}/api/v2/products/?name=${productName}" \
-                                 
-  //                       """,
-  //                       returnStdout: true
-  //                       )
-    // productId = readJson(text: productId).results[0]?.id ?: null
-    def responseProduct = sh(
-    script: """curl -s -k -H "Authorization: Token ${DOJO_KEY}" "${DEFECTDOJO_BASE_URL}/api/v2/products/?name=${productName}" """,
-    returnStdout: true
-)
+def ImportReport(fullPath, imageTag, component) {
+  String productName = "${fullPath}-${component}"
+  String engagementName = "${component}:${imageTag}"
 
-def productId = (responseProduct =~ /"id":\s*(\d+)/)[0][1] ?: null
-   
-   if (productId == "null" || productId == "") {
-                        echo "Product not found. Creating..."
-                        productId = sh(
+  def productId = sh(
+                        script: """
+                            curl -s -k -H "Authorization: Token ${DOJO_KEY}" \
+                                 "${DEFECTDOJO_BASE_URL}/api/v2/products/?name=${productName}" \
+
+                        """,
+                        returnStdout: true
+                        )
+  productId = readJSON(text: productId).results[0]?.id ?: null
+//   def responseProduct = sh(
+//     script: """curl -s -k -H "Authorization: Token ${DOJO_KEY}" "${DEFECTDOJO_BASE_URL}/api/v2/products/?name=${productName}" """,
+//     returnStdout: true
+// )
+
+//   def productId = (responseProduct =~ /"id":\s*(\d+)/)[0][1] ?: null
+
+  if (productId == 'null' || productId == '') {
+    echo 'Product not found. Creating...'
+    productId = sh(
                             script: """
                                 curl -s -k -X POST "${DEFECTDOJO_BASE_URL}/api/v2/products/" \
                                     -H "Authorization: Token ${DOJO_KEY}" \
@@ -32,23 +32,23 @@ def productId = (responseProduct =~ /"id":\s*(\d+)/)[0][1] ?: null
                             """,
                             returnStdout: true
                         )
-                    }
+  }
 
-  // def engagementId = sh(
-  //                       script: """
-  //                           curl -s -k -H "Authorization: Token ${DOJO_KEY}" \
-  //                                "${DEFECTDOJO_BASE_URL}/api/v2/engagements/?name=${engagementName}&product=${productId}" 
-  //                       """,
-  //                       returnStdout: true
-  //                      )
-  //     engagementId = readJSON(text: engagementId).results[0]?.id ?: null
-   def responseEngagement = sh(
-    script: """curl -s -k -H "Authorization: Token ${DOJO_KEY}" "${DEFECTDOJO_BASE_URL}/api/v2/engagements/?name=${engagementName}&product=${productId}" """,
-    returnStdout: true)
-    def engagementId = (responseEngagement =~ /"id":\s*(\d+)/)[0][1] ?: null
-   if (engagementId == null || engagementId == "") {
-                        echo "Engagement not found. Creating..."
-                        engagementId = sh(
+  def engagementId = sh(
+                        script: """
+                            curl -s -k -H "Authorization: Token ${DOJO_KEY}" \
+                                 "${DEFECTDOJO_BASE_URL}/api/v2/engagements/?name=${engagementName}&product=${productId}"
+                        """,
+                        returnStdout: true
+                       )
+      engagementId = readJSON(text: engagementId).results[0]?.id ?: null
+  // def responseEngagement = sh(
+  //   script: """curl -s -k -H "Authorization: Token ${DOJO_KEY}" "${DEFECTDOJO_BASE_URL}/api/v2/engagements/?name=${engagementName}&product=${productId}" """,
+  //   returnStdout: true)
+  // def engagementId = (responseEngagement =~ /"id":\s*(\d+)/)[0][1] ?: null
+  if (engagementId == null || engagementId == '') {
+    echo 'Engagement not found. Creating...'
+    engagementId = sh(
                             script: """
                                 curl -s -k -X POST "${DEFECTDOJO_BASE_URL}/api/v2/engagements/" \
                                     -H "Authorization: Token ${DOJO_KEY}" \
@@ -62,10 +62,10 @@ def productId = (responseProduct =~ /"id":\s*(\d+)/)[0][1] ?: null
                             """,
                             returnStdout: true
                         ).trim()
-                    }
-      withCredentials([string(credentialsId: DOJO_KEY, variable: 'DOJO_KEY')]) {
-          //SonarQube Scan Source Code
-          sh """
+  }
+  withCredentials([string(credentialsId: DOJO_KEY, variable: 'DOJO_KEY')]) {
+    //SonarQube Scan Source Code
+    sh """
 
           curl -k -X POST "${DEFECTDOJO_BASE_URL}/api/v2/reimport-scan/" \
             -H "Authorization: Token ${DOJO_KEY}" \
@@ -80,8 +80,8 @@ def productId = (responseProduct =~ /"id":\s*(\d+)/)[0][1] ?: null
             -F close_old_findings=true \
             -F auto_create_context=true
         """
-          //Trivy Scan Dependency
-          sh """
+    //Trivy Scan Dependency
+    sh """
           curl -k -X POST "https://defectdojo.phurithat.site/api/v2/reimport-scan/" \
             -H "Authorization: Token ${DOJO_KEY}" \
             -F scan_type="CycloneDX Scan" \
@@ -111,5 +111,5 @@ def productId = (responseProduct =~ /"id":\s*(\d+)/)[0][1] ?: null
             -F close_old_findings=true \
             -F auto_create_context=true
         """
-}
+  }
 }
