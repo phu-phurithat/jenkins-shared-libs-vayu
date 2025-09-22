64c796869520128404f5481dc0cf4883ea717e6a
Thun650710553 <116154739+Thun650710553@users.noreply.github.com>
2025-09-22 07:20:43 +0000

    debug go

--- src/devops/v1/PodTemplate.groovy
+++ src/devops/v1/PodTemplate.groovy
@@ -94,7 +94,7 @@ class PodTemplate implements Serializable {
   PodTemplate addGo(String goVersion) {
   
     goVersion = goVersion.tokenize('.') // eg. 1.23 -> [1, 23]
-    echo "goVersion = ${goVersion}"
+    script """ echo "goVersion = ${goVersion} """
     String goImage = "golang:${goVersion[0]}.${goVersion[1]}-alpine"
     addContainerIfMissing([
       name           : 'golang',
