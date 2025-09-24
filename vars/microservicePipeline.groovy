0440e00aa4f4d31ecbc37201ee9d10d11ab23c95
Phurithat Komkham <99815333+phu-phurithat@users.noreply.github.com>
2025-09-24 09:13:47 +0000

    make parallel

--- vars/microservicePipeline.groovy
+++ vars/microservicePipeline.groovy
@@ -1,43 +1,33 @@
 import devops.v1.*
 
 def call(args) {
-  // args only contain
-  // DEPLOYMENT_REPO, TRIGGER_TOKEN, MICROSERVICE_NAME, BRANCH (optional), AUTO_DEPLOY, TARGET_ENV
-  // IMAGE_TAG (optional)
-
-  
-  
-  
-  
-   
-   
-  
-  
+  // args only contain:
+  // DEPLOYMENT_REPO, TRIGGER_TOKEN, MICROSERVICE_NAME, BRANCH (optional),
+  // AUTO_DEPLOY, TARGET_ENV, IMAGE_TAG (optional)
 
   // Helper classes
-  def pt = new PodTemplate()
+  def pt          = new PodTemplate()
   def credManager = new CredManager()
-  def builder = new Builder()
-  def scanner = new SecurityScanner()
-  def defectdojo = new DefectDojoManager()
-  def prep = new Preparer()
-  def dm = new DeployManager()
-  def gitm = new GitManager()
+  def builder     = new Builder()
+  def scanner     = new SecurityScanner()
+  def defectdojo  = new DefectDojoManager()
+  def prep        = new Preparer()
+  def dm          = new DeployManager()
+  def gitm        = new GitManager()
 
   // Variables
-  def config = [:]
-  def properties = [:]
-  String appName = ''
+  def config          = [:]
+  def properties      = [:]
+  String appName      = ''
   String microserviceRepo = ''
-  String fullImageName = ''
-  String imageTag = ''
-  def fullPath = ''
-  def component = ''
-  def productName = '' //"${fullPath}-${component}"
-  def engagementName = '' //"${component}:${imageTag}" 
- def sonarProjectKey = '' //{fullPath}-{Component}  
- def sonarProjectName = ''
- 
+  String fullImageName    = ''
+  String imageTag         = ''
+  def fullPath        = ''
+  def component       = ''
+  def productName     = '' // "${fullPath}-${component}"
+  def engagementName  = '' // "${component}:${imageTag}"
+  def sonarProjectKey = '' // "{fullPath}-{Component}"
+  def sonarProjectName = ''
 
   // ------------------- Prep on a controller/agent -------------------
   node('master') { // change label as needed
@@ -53,12 +43,11 @@ def call(args) {
       }
 
       stage('Read Configuration from "config.yaml"') {
-        String configPath = './config.yaml'
+        String configPath    = './config.yaml'
         String configContent = readFile(file: configPath, encoding: 'UTF-8')
 
         if (configContent?.trim()) {
           config = readYaml(text: configContent)
-        // echo config.toString()
         } else {
           error "Configuration file not found or empty at ${configPath}"
         }
@@ -69,26 +58,24 @@ def call(args) {
     dir('src') {
       stage('Checkout Microservice Code for Preparation') {
         microserviceRepo = config.kinds.deployments[args.MICROSERVICE_NAME]
-         if (microserviceRepo) {
-        def matcher = (microserviceRepo =~ /github\.com\/(.*)\.git/)
-        if (matcher.find()) {
-        fullPath = matcher.group(1)
-        component = fullPath.tokenize('/')[-1].replace('.git', '')
-        fullPath = fullPath.replace('/', '-')
-        productName = "${fullPath}-${component}"
-        engagementName = "${component}:${imageTag}"
-        sonarProjectKey = "${fullPath}-${component}"
-        sonarProjectName = sonarProjectKey
-        echo "fullPath:${fullPath} component: ${component} Product: ${fullPath}-${component}"
-        echo  "and Engagement: ${component}:${imageTag}"
-    } else {
-        error("Could not parse repo URL: ${microserviceRepo}")
-    }
-} else {
-    error("Microservice '${args.MICROSERVICE_NAME}' not found in configuration.")
-}
-        if (!microserviceRepo) {
-          error "Microservice '${args.MICROSERVICE_NAME}' not found in configuration."
+        if (microserviceRepo) {
+          def matcher = (microserviceRepo =~ /github\.com\/(.*)\.git/)
+          if (matcher.find()) {
+            fullPath        = matcher.group(1)
+            component       = fullPath.tokenize('/')[-1].replace('.git', '')
+            fullPath        = fullPath.replace('/', '-')
+            productName     = "${fullPath}-${component}"
+            engagementName  = "${component}:${imageTag}"
+            sonarProjectKey = "${fullPath}-${component}"
+            sonarProjectName = sonarProjectKey
+
+            echo "fullPath:${fullPath} component:${component} Product:${productName}"
+            echo "and Engagement:${engagementName}"
+          } else {
+            error("Could not parse repo URL: ${microserviceRepo}")
+          }
+        } else {
+          error("Microservice '${args.MICROSERVICE_NAME}' not found in configuration.")
         }
 
         echo "Cloning microservice repository: ${microserviceRepo}"
@@ -107,8 +94,9 @@ def call(args) {
         // imageTag = args.IMAGE_TAG ?: sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
         imageTag = 'latest'
         fullImageName = args.TARGET_ENV in ['prod', 'production'] ?
-        "${config.registry.prod}/${args.MICROSERVICE_NAME}:${imageTag}" :
-        "${config.registry.nonprod}/${args.MICROSERVICE_NAME}:${imageTag}"
+          "${config.registry.prod}/${args.MICROSERVICE_NAME}:${imageTag}" :
+          "${config.registry.nonprod}/${args.MICROSERVICE_NAME}:${imageTag}"
+
         prep.validateProperties(properties)
         prep.getConfigSummary(args, config, properties, fullImageName)
       }
@@ -123,34 +111,36 @@ def call(args) {
   podTemplate(yaml: pt.toString()) {
     node(POD_LABEL) {
       stage('Checkout Source Code Repository') {
-        // Re-clone to ensure clean state inside pod
         git url: microserviceRepo, branch: args.BRANCH
       }
 
       String build_tool = properties.build_tool.toLowerCase()
-      String language = properties.language.toLowerCase()
+      String language   = properties.language.toLowerCase()
       echo "build_tool = ${build_tool}"
 
       stage('Build') {
         builder.Compile(build_tool)
       }
+      
 
-      stage('Sorce Code Scan') {
-        container('sonarqube') {
-          scanner.SorceCodeScan(sonarProjectKey, sonarProjectName,language)
+      parallel(
+        "Source Code Scan": {
+          container('sonarqube') {
+            scanner.SorceCodeScan(sonarProjectKey, sonarProjectName, language)
+          }
+        },
+        "Build Docker Image": {
+          container('buildkit') {
+            builder.BuildImage(fullImageName)
+          }
+        },
+        "Dependencies Scan": {
+          container('trivy') {
+            scanner.DependenciesScan()
+          }
         }
-      }
+      )
 
-      stage('Build Docker Image') {
-        container('buildkit') {
-          builder.BuildImage( fullImageName)
-        }
-      }
-      stage('Dependencies Scan') {
-        container('trivy') {
-          scanner.DependenciesScan()
-        }
-      }
       stage('Image Scan') {
         container('trivy') {
           scanner.ImageScan(fullImageName)
@@ -161,22 +151,21 @@ def call(args) {
         defectdojo.ImportReport(productName, engagementName)
       }
 
-      if (args.AUTO_DEPLOY in [true, 'true'] ) {
+      if (args.AUTO_DEPLOY in [true, 'true']) {
         dir('deployment') {
           stage('Checkout Deployment Repository') {
-            // Re-clone to ensure clean state inside pod
             git url: args.DEPLOYMENT_REPO, branch: args.BRANCH
           }
 
           String kubeconfigCred = config.environments[args.TARGET_ENV].cluster.toLowerCase()
-          String namespace    = config.environments[args.TARGET_ENV].namespace.toLowerCase()
-          String valuePath   = "./deployments/${args.TARGET_ENV}/${args.MICROSERVICE_NAME}.yaml"   // path where your Helm chart lives
-          String helmRelease = args.DEPLOYMENT_REPO.tokenize('/').last().replace('.git', '').toLowerCase()
+          String namespace      = config.environments[args.TARGET_ENV].namespace.toLowerCase()
+          String valuePath      = "./deployments/${args.TARGET_ENV}/${args.MICROSERVICE_NAME}.yaml"
+          String helmRelease    = args.DEPLOYMENT_REPO.tokenize('/').last().replace('.git', '').toLowerCase()
 
           // Update Helm values with the new image
           dm.updateHelmValuesFile(valuePath, fullImageName)
 
-          // Setup Helm (remove default stable repo and add ours)
+          // Setup Helm
           gitm.setupHelm()
 
           // Deploy via Helm if auto-deploy is enabled
@@ -190,7 +179,6 @@ def call(args) {
             )
             isDeploySuccess = true
           } catch (Exception e) {
-            isDeploySuccess = false
             echo "Deployment to ${args.TARGET_ENV} failed. Attempting rollback..."
             dm.rollbackHelm(helmRelease, namespace, kubeconfigCred)
           }
@@ -209,5 +197,5 @@ def call(args) {
       }
     }
   }
-// ------------------- End of podTemplate -------------------
+  // ------------------- End of podTemplate -------------------
 }
